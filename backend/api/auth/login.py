6ece22686e699df1db77a913f5b911951ff44e5e
Kirill <userkirill609@gmail.com>
2023-10-28 21:22:51 +0300

    some refactoring. Add token service class for awesome operations with refresh tokens

--- backend/api/auth/login.py
+++ backend/api/auth/login.py
@@ -10,9 +10,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import Response
 
 from backend.core import settings
-from backend.db.crud.token_crud import CrudToken
-from backend.db.models import User, Token
-from backend.schemas.token import ShowTokenAfterInsert
+from backend.db.models import User
+from api.services import TokenService
 from .utils import authenticate_user, _get_user_by_email_for_auth, get_current_user_from_token
 from backend.db.connect_db import get_db
 from backend.core.security import create_access_token, create_refresh_token, verify_refresh_token
@@ -23,48 +22,16 @@ login_router = APIRouter(prefix='/login')
 async def _unset_all_cookies(response: Response) -> str:
     response.delete_cookie(key="access_token")
     response.delete_cookie(key="refresh_token_uuid")
+    response.delete_cookie(key="login_status")
     return "successful unset"
 
 
-async def _insert_refresh_token_into_db(token: str, user: User, session: AsyncSession) -> ShowTokenAfterInsert:
-    async with session.begin():
-        c = CrudToken(session=session)
-        token = await c.create(token=token, user_id=user.user_id)
-        return ShowTokenAfterInsert(
-            token_id=token.token_id,
-            user_id=token.user_id,
-            token=token.token,
-            date_created=token.date_created
-        )
-
-
-async def _get_token_from_db_with_user(user: User, session: AsyncSession) -> Token:
-    async with session.begin():
-        c = CrudToken(session=session)
-        token = await c.get_token_by_user(user)
-        return token
-
-
-async def _get_token_from_db_with_id(token_id: UUID, session: AsyncSession) -> Token:
-    async with session.begin():
-        c = CrudToken(session=session)
-        token = await c.get_token_by_id(token_id)
-        return token
-
-
-async def _delete_token(user: User = None, token_id: UUID = None, session: AsyncSession = None):
-    async with session.begin():
-        c = CrudToken(session=session)
-        await c.delete(user, token_id)
-        return "Token successful delete"
-
-
-# @login_router.post("/token", response_model=TokenAfterLogin)
 @login_router.post("/token")
 async def login_for_access_token(
         response: Response,
         form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
 ):
+    token_worker = TokenService(db)
     user = await authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
@@ -77,14 +44,18 @@ async def login_for_access_token(
         expires_delta=access_token_expires,
     )
     response.set_cookie(key="access_token", value=f"{access_token}", httponly=True)
+
+    response.set_cookie(key="login_status", value="true")
+
     refresh_token = await create_refresh_token(
         data={"sub": user.email}
     )
-    token_in_db = await _get_token_from_db_with_user(user=user, session=db)
+    token_in_db = await token_worker.get_token_from_db_with_user(user=user)
     if token_in_db is None:
-        token_in_db = await _insert_refresh_token_into_db(refresh_token, user, db)
+        token_in_db = await token_worker.insert_refresh_token_into_db(refresh_token, user)
 
     response.set_cookie(key="refresh_token_uuid", value=f'{token_in_db.token_id}', httponly=True)
+
     return {
         "refresh_token_uuid": token_in_db.token_id,
         "message": "User Logged in Successfully."
@@ -95,7 +66,8 @@ async def login_for_access_token(
 async def get_new_access_token(response: Response,
                                token_uuid: UUID, db: AsyncSession = Depends(get_db),
                                current_user: User = Depends(get_current_user_from_token)):
-    token = await _get_token_from_db_with_id(token_uuid, db)
+    token_worker = TokenService(db)
+    token = await token_worker.get_token_from_db_with_id(token_uuid)
     refresh_data = await verify_refresh_token(token.token)
     user_email = refresh_data.get("sub")
 
@@ -104,9 +76,9 @@ async def get_new_access_token(response: Response,
     new_access_token = await create_access_token(refresh_data)
     new_refresh_token = await create_refresh_token(data={"sub": user_email})
 
-    await _delete_token(token_id=token.token_id, session=db)
+    await token_worker.delete_token(token_id=token.token_id)
 
-    token_in_db = await _insert_refresh_token_into_db(token=new_refresh_token, user=user, session=db)
+    token_in_db = await token_worker.insert_refresh_token_into_db(token=new_refresh_token, user=user)
 
     response.delete_cookie(key="access_token")
     response.delete_cookie(key="refresh_token_uuid")
